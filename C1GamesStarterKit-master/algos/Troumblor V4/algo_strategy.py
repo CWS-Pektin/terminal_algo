0265a5d4ce501cef7d6b1a0018af1305c1c80271
Trouble <paradogxlex@AlexandersMBP2.lan>
2018-11-17 02:56:07 +0100

    Troumblor V4 Finish
    Next stop Linezor

--- C1GamesStarterKit-master/algos/Troumblor V4/algo_strategy.py
+++ C1GamesStarterKit-master/algos/Troumblor V4/algo_strategy.py
@@ -31,19 +31,14 @@ class AlgoStrategy(gamelib.AlgoCore):
         game_state.suppress_warnings(True)
         #______________
         
-        switch = True
-
         FILTER_DEPLOY_LOCATION = [[ 4, 12],[ 23, 12],[ 4, 11],[ 5, 11],[ 6, 11],[ 21, 11],[ 22, 11],[ 23, 11],[ 7, 9],[ 20, 9],[ 7, 8],[ 8, 8],[ 9, 8],[ 18, 8],[ 19, 8],[ 20, 8],[ 10, 6],[ 17, 6],[ 10, 5],[ 11, 5],[ 12, 5],[ 15, 5],[ 16, 5],[ 17, 5],[ 13, 3],[ 14, 3]]
         ENCRYPTOR_DEPLOY_LOCATION = [[ 4, 13],[ 23, 13],[ 7, 10],[ 20, 10],[ 10, 7],[ 17, 7],[ 13, 4],[ 14, 4],[ 13, 2],[ 14, 2]]
         DESTRUCTOR_DEPLOY_LOCATION = [[ 3, 13],[ 24, 13],[ 6, 10],[ 21, 10],[ 9, 7],[ 18, 7],[ 12, 4],[ 15, 4],[ 13, 2],[ 14, 2]]
 
-        if switch:
+        if game_state.turn_number % 2:
             self.deploy_information(game_state, [13, 0], PING, 5 )
-            switch = False
-
-        elif not switch:
-            self.deploy_information(game_state, [14, 0], PING, 5)
-            switch = True
+        else:
+            self.deploy_information(game_state, [14, 0], PING, 5 )
 
         
         self.deploy_fireWall(game_state, DESTRUCTOR_DEPLOY_LOCATION, DESTRUCTOR)
@@ -62,14 +57,14 @@ class AlgoStrategy(gamelib.AlgoCore):
         for location in deploy_location:
             if game_state.get_resource(game_state.CORES) >= game_state.type_cost(unit):
                 if game_state.can_spawn(unit, location):
-                    gamelib.debug_write(f"Eine Firewall: {unit} wurde auf der Position {location} für {game_state.type_cost(unit)} Cores gebaut.")
                     game_state.attempt_spawn(unit, location)
+                    #gamelib.debug_write(f"Eine Firewall: {unit} wurde auf der Position {location} für {game_state.type_cost(unit)} Cores gebaut.")
 
     def deploy_information(self, game_state, location, unit, unitCount):
         #deploy a given count of information-units on spezific location
-        if game_state.get_resource(game_state.BITS) >= game_state.type_cost(unit, int(round(unit*unitCount))):
-            if game_state.can_spawn(unit, location):
-                game_state.attempt_spawn(unit, location, unitCount)
+        if game_state.can_spawn(unit, location):
+            game_state.attempt_spawn(unit, location, unitCount)
+            gamelib.debug_write(f"Es wurden {unitCount} Informations-Einheiten: {unit} wurde auf der Position {location} für {int(game_state.type_cost(unit)*unitCount)} Bits gebaut")
 
 
 if __name__ == "__main__":
